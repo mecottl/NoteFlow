d2970013ba50a89950af43bb111e20d8dc3daa43
mecottl <gerardomecott@outlook.com>
2025-08-07 10:55:34 -0600

    reparando ruteo de vercel

--- Backend/index.js
+++ Backend/index.js
@@ -51,7 +51,7 @@ app.get('/api', (req, res) => {
 if (config.IS_PRODUCTION) {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
   
-  app.get('*', (req, res) => {
+  app.get('/.*/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
